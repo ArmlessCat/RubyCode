42c53314b742192482c7e3b1d7b77a4183018858
Reid <reidenglandj@gmail.com>
2013-01-28 21:39:07 -0600

    Added RubyCode file

---
+++ Desktop/RubyCode/RubyCode/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyCode::Application.config.secret_token = '0263e504ac3f046e2b2257427c40b4b01d16e8927f0690e7d2eaca02b8f0f46ce201013edcda6c7d8fe6a69cd4fd8639a5c6ec7b50d7fde054e6ba77064e399e'
