1dd70a0ffc7c18b3036ba682bb129f1b44111cda
Reid <reidenglandj@gmail.com>
2013-02-15 22:26:42 -0600

    finished demo app

---
+++ Desktop/RubyScaffold/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyScaffold::Application.config.secret_token = 'eeb8d35745b671f7f72e1d83a42b779eba22758279230eac4957261ef076cee5c8279b116dea6b42c968b19da16a91019e32283f223280e203e2a190962b3b57'
